4ff2266ca9beeca1211090f96aaf7dc1e18519a4
kudzurunner <sdio@tuta.io>
2019-01-26 00:12:36 +0300

    fixed username

--- build.py
+++ build.py
@@ -2,7 +2,7 @@ from cpt.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username="KudzuRunner")
+    builder = ConanMultiPackager(username="kudzurunner")
     builder.add(settings={"arch": "x86_64", "os": "Windows"}, options={}, env_vars={}, build_requires={})
     builder.add(settings={"arch": "x86", "os": "Windows"}, options={}, env_vars={}, build_requires={})
     builder.run()
